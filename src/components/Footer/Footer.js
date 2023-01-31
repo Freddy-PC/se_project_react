6337b75db4f161efd418c54c3acdc87b9c25e1a4
Freddy PC <freddythepc@gmail.com>
2023-01-31 15:58:08 -0500

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+// import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__text">Developed by Freddy Perez-Camacho</p>
+      <p className="footer__text">2022</p>
+    </div>
+  );
+}
+
+export default Footer;
