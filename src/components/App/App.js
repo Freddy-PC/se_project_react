303197d947d508e4ac5a3e9a72e7584b4bb1d27a
Freddy PC <freddythepc@gmail.com>
2023-02-01 14:30:24 -0500

    Connect weatherData within files for API calls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
-import "./App.css";
+import {
+  latitude,
+  longitude,
+  key,
+  weatherForecast,
+} from "../../utils/constants";
+import { getWeather, setDataFromWeatherApi } from "../../utils/weatherApi";
 
-function App() {
+const App = () => {
+  const [weatherData, setWeatherData] = useState({});
+
+  // Retreive data from weatherAPI
+  useEffect(() => {
+    if (latitude && longitude) {
+      // Get weather info
+      getWeather(latitude, longitude, key)
+        .then((data) => {
+          // Update weather info
+          // Console.log returns all data
+          setWeatherData(setDataFromWeatherApi(data));
+        })
+        .catch((err) => console.log(err));
+    }
+  }, []);
+  // Only called once to prevent Error:
+
   return (
     <div className="page">
       <div className="page__container">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
         <Footer />
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Main.css";
+import WeatherCard from "../WeatherCard/WeatherCard";
 
-function Main() {
+function Main({ weatherData }) {
+  const actualWeather = weatherData.temperature;
   return (
     <main className="main">
-      {/* <WeatherCard /> */}
-      <h3 className="main__header">Today is 75° F / You may want to wear:</h3>
+      <WeatherCard weatherData={weatherData} />
+      <h3 className="main__header">
+        {/* Auto updates weather temp */}
+        Today is {Math.round(actualWeather)}°F / You may want to wear:
+      </h3>
       <ul className="main__items">{/* cards */}</ul>
     </main>
   );
