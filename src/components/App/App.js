e3cb19012d0456ab0b3a8f3d7aa5d2bcd2b3f179
Freddy PC <freddythepc@gmail.com>
2023-02-02 17:30:17 -0500

    Add notes, useState hook to connect selectedCard to ItemModal and open image Modals in Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,13 +7,27 @@ import {
   latitude,
   longitude,
   key,
-  weatherForecast,
+  // weatherForecast,
 } from "../../utils/constants";
 import { getWeather, setDataFromWeatherApi } from "../../utils/weatherApi";
 import { defaultClothingItems } from "../../utils/clothingItems";
+import ItemModal from "../ItemModal/ItemModal";
 
 const App = () => {
   const [weatherData, setWeatherData] = useState({});
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+
+  // When image clicked...(clothing property accessed via 'card')
+  const handleClick = (card) => {
+    setSelectedCard(card);
+    setActiveModal("preview");
+  };
+
+  // On Modal when button clicked not 'preview'
+  const closeAllModals = () => {
+    setActiveModal("");
+  };
 
   // Retreive data from weatherAPI
   useEffect(() => {
@@ -27,16 +41,22 @@ const App = () => {
         })
         .catch((err) => console.log(err));
     }
-  }, []);
-  // Only called once to prevent Error: 429
+  }, []); // Only called once to prevent Error: 429
 
   return (
     <div className="page">
       <div className="page__container">
         <Header weatherData={weatherData} />
-        <Main weatherData={weatherData} cards={defaultClothingItems} />
+        <Main
+          weatherData={weatherData}
+          cards={defaultClothingItems}
+          cardClick={handleClick}
+        />
         <Footer />
       </div>
+      {activeModal === "preview" && (
+        <ItemModal card={selectedCard} onClose={closeAllModals} />
+      )}
     </div>
   );
 };
