970b9a04457089836b490f5275d62b93a1363def
Freddy PC <freddythepc@gmail.com>
2023-02-02 17:19:59 -0500

    Add structure for HTML, cinnect card properties and close function and add styles

---
+++ src/components/ItemModal/ItemModal.js
@@ -0,0 +1,25 @@
+import "./ItemModal.css";
+
+function ItemModal({ card, onClose }) {
+  return (
+    <div className="item-modal">
+      <div className="item-modal__container">
+        <button
+          onClick={onClose}
+          type="button"
+          className="item-modal__close-button"
+          alt="close button"
+        />
+        <img
+          src={card.link}
+          alt={`${card.name}`}
+          className="item-modal__image"
+        />
+        <p className="item-modal__title">{card.name}</p>
+        <p className="item-modal__description">Weather: {card.weather}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ItemModal;
