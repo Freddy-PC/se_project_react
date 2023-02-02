2bd7aa020c554c66397efcdadc4c5907c2334660
Freddy PC <freddythepc@gmail.com>
2023-02-01 20:43:38 -0500

    Add structure for filtering cards testing card styles on page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,31 @@
 import React from "react";
 import "./Main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
 
-function Main({ weatherData }) {
+function Main({ weatherData, defaultClothingItems }) {
   const currentWeather = weatherData.temperature;
+
+  // Filter cards via temp
+  //   if (currentWeather >= 86) {
+  //     return "hot";
+  //   } else if (currentWeather >= 66 && currentWeather <= 85) {
+  //     return "warm";
+  //   } else if (currentWeather <= 65) {
+  //     return "cold";
+  //   }
+
   return (
     <main className="main">
       <WeatherCard weatherData={weatherData} />
       <h3 className="main__header">
         Today is {Math.round(currentWeather)}°F / You may want to wear:
       </h3>
-      <ul className="main__items">{/* cards */}</ul>
+      <ul className="main__items">
+        {defaultClothingItems.map((card) => (
+          <ItemCard clothing={card} key={card._id} />
+        ))}
+      </ul>
     </main>
   );
 }
