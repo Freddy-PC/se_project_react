74c0b2d60aa53140edb4c2f419d07702f619c8f4
Freddy PC <freddythepc@gmail.com>
2023-02-02 17:26:27 -0500

    Connect clothing property and cardClick for image

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,12 +1,20 @@
 import "./ItemCard.css";
 
-function ItemCard({ clothing }) {
+function ItemCard({ clothing, cardClick }) {
   return (
     // List of cards
     <li className="card">
       <div className="card__wrapper">
         <h3 className="card__title">{clothing.name}</h3>
-        <img className="card__image" src={clothing.link} alt={clothing.name} />
+        <img
+          // When clicked gets clothig data
+          onClick={() => {
+            cardClick(clothing);
+          }}
+          className="card__image"
+          src={clothing.link}
+          alt={clothing.name}
+        />
       </div>
     </li>
   );
