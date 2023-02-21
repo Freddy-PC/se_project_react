4a3e06f40947cfd8c8e27c9275e614db7b6b1411
Freddy PC <freddythepc@gmail.com>
2023-02-21 16:44:56 -0500

    Access data via useContext and change values according to currentTempUnit

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,12 +1,21 @@
 import "./WeatherCard.css";
 import cloudyday from "../../images/cloudyday.svg"; // Delete later...
+import { useContext } from "react";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 
 function WeatherCard({ weatherData }) {
+  // Access data from Context
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+  // Return nothing if no weatherData
+  if (!weatherData) return null;
+
   return (
     <div className="weather-card">
       <h2 className="weather-card__temp">
         {/* Auto updates temp */}
-        {Math.round(weatherData.temperature)}°F
+        {currentTemperatureUnit === "F"
+          ? weatherData.temperatureF
+          : weatherData.temperatureC}
       </h2>
       <div className="weather-card__wrapper">
         <img className="weather-card__image" src={cloudyday} alt="..." />
