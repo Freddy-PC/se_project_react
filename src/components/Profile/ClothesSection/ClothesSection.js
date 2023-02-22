97910ecce5245c4fb9074a28956057f1f14789e0
Freddy PC <freddythepc@gmail.com>
2023-02-21 19:28:03 -0500

    Add structure to component

--- src/components/Profile/ClothesSection/ClothesSection.js
+++ src/components/Profile/ClothesSection/ClothesSection.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./ClothesSection.js";
 
-function ClothesSection({ cards, cardClick }) {}
+function ClothesSection({ cards, cardClick }) {
+  return (
+    <div className="clothes">
+      <div className="clothes__header">
+        <p className="clothes__title">Your items</p>
+        <button className="clothes__add-button">+Add new</button>
+      </div>
+      <ul className="clothes__items"></ul>
+    </div>
+  );
+}
 
 export default ClothesSection;
