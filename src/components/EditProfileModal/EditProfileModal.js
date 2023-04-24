4e419bfc904d17d5be756c0b3bf0ad832297349f
Freddy PC <freddythepc@gmail.com>
2023-04-24 12:26:29 -0400

    Add format for editing profile

---
+++ src/components/EditProfileModal/EditProfileModal.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import "../ModalWithForm/ModalWithForm.js";
+
+const EditProfileModal = ({ onClose, handleEditProfile }) => {
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  const handleName = (evt) => {
+    setName(evt.target.value);
+  };
+  const handleAvatar = (evt) => {
+    setAvatar(evt.target.value);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleEditProfile(name, avatar);
+    history.push("/profile");
+  }
+
+  return (
+    <>
+      <ModalWithForm
+        title="Change Profile Data"
+        buttonText="Save changes"
+        onClose={onClose}
+        handleSubmit={handleSubmit}
+      >
+        <label className="form__heading">Name</label>
+        <input
+          className="form__input form__input_type_image"
+          name="Name"
+          type="text"
+          placeholder="Name"
+          id="Name"
+          required
+          onChange={handleName}
+          value={name}
+        />
+        <label className="form__heading">Avatar URL</label>
+        <input
+          className="form__input form__input_type_image"
+          name="Avatar URL"
+          type="text"
+          placeholder="Avatar URL"
+          id="avatar-URL"
+          required
+          onChange={handleAvatar}
+          value={avatar}
+        />
+      </ModalWithForm>
+    </>
+  );
+};
+
+export default EditProfileModal;
