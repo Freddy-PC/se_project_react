862de49141a108855ee20f86d474e8ac7072e23a
Freddy PC <freddythepc@gmail.com>
2023-12-19 16:53:52 -0500

    feat: comapare values from api and object to assign day images

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,5 +1,5 @@
 import "./WeatherCard.css";
-import cloudyday from "../../images/cloudyday.svg"; // Delete later...
+import { weatherForecast } from "../../utils/constants";
 import { useContext } from "react";
 import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 
@@ -8,6 +8,25 @@ function WeatherCard({ weatherData }) {
   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
   // Return nothing if no weatherData
   if (!weatherData) return null;
+
+  console.log(weatherData);
+
+  // Make a variable from API value to pass as a value
+  const currentForecastName = weatherData.forecast;
+
+  ///////////////////////////////////////////////////
+  // // Logic if it's day based on unix time values
+  // const dayTime = weatherData.unixTime < weatherData.sunset;
+
+  /* Find the same weather value from the array of objects 
+     and the forecast (API) array */
+  const currentWeather = weatherForecast.find(
+    (item) => item.name === currentForecastName
+  );
+
+  /* If the weather value in the array of objects is found
+     get the image path from the array of objects */
+  const weatherImagePath = currentWeather ? currentWeather.image : null;
 
   return (
     <div className="weather-card">
@@ -18,7 +37,11 @@ function WeatherCard({ weatherData }) {
           : weatherData.temperatureC}
       </h2>
       <div className="weather-card__wrapper">
-        <img className="weather-card__image" src={cloudyday} alt="..." />
+        <img
+          className="weather-card__image"
+          src={weatherImagePath}
+          alt="weather icon"
+        />
       </div>
     </div>
   );
