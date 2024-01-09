46d54b9c82c498f530ff86581daed0bed361cb65
Freddy PC <freddythepc@gmail.com>
2024-01-09 16:28:20 -0500

    refactor: template for error message in input field

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -7,18 +7,50 @@ const LoginModal = ({ onClose, handleSignin, handleRedirect, isLoading }) => {
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  console.log(password);
+  console.log(password.length);
+  console.log(passwordError);
+
+  const emailRegex = /\S+@\S+\.\S+/;
+
+  const validateEmail = () => {
+    if (email.trim() === "" || !emailRegex.test(email)) {
+      setEmailError("Email is required");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const validatePassword = () => {
+    if (password.length < 5) {
+      setPasswordError("Password must be at least 5 characters");
+    } else {
+      setPasswordError("");
+    }
+  };
 
   const handleEmail = (evt) => {
     setEmail(evt.target.value);
+    validateEmail();
   };
   const handlePassword = (evt) => {
     setPassword(evt.target.value);
+    validatePassword(evt);
   };
 
   function handleSubmit(e) {
     e.preventDefault();
-    handleSignin(email, password);
-    history.push("/profile");
+
+    validateEmail();
+    validatePassword();
+    // Check for errors before submitting
+    if (!emailError && !passwordError) {
+      handleSignin(email, password);
+      history.push("/profile");
+    }
   }
 
   return (
@@ -43,6 +75,7 @@ const LoginModal = ({ onClose, handleSignin, handleRedirect, isLoading }) => {
         minLength="1"
         maxLength="30"
       />
+      {emailError && <p className="error-message">{emailError}</p>}
       <label className="form__heading">Password</label>
       <input
         className="form__input form__input_type_image"
@@ -55,6 +88,7 @@ const LoginModal = ({ onClose, handleSignin, handleRedirect, isLoading }) => {
         value={password}
         minLength="5"
       />
+      {passwordError && <p className="error-message">{passwordError}</p>}
     </ModalWithForm>
   );
 };
